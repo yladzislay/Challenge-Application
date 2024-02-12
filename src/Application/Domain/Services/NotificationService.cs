2a9b5b854bd010f91ed98df26c381debc01a99d7
Uladzislau Darazhei <yladzislay@gmail.com>
2024-02-13 01:20:20 +0300

    Define and use NotificationTemplate.

--- src/Application/Domain/Services/NotificationService.cs
+++ src/Application/Domain/Services/NotificationService.cs
@@ -4,151 +4,100 @@ namespace Application.Domain.Services
 {
     public class NotificationService : INotifications
     {
+        private readonly static string NotificationTemplate = "[Notification]: {0}" 
+                                                              + Environment.NewLine
+                                                              + "---[Details]: {1}" 
+                                                              + Environment.NewLine
+                                                              + "--[For user]: {2}";
+
         public void NotifyApproachingDepositLimit(string emailAddress, decimal currentDepositAmount, decimal limit)
         {
-            Console.WriteLine("[Notification]: Approaching deposit limit!"
-                              + Environment.NewLine
-                              + $"-[Current deposit amount]: {currentDepositAmount}"
-                              + Environment.NewLine
-                              + $"-[Limit]: {limit} "
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Approaching deposit limit! Current deposit amount: {currentDepositAmount}, Limit: {limit}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyExceededDepositLimit(string emailAddress, decimal currentDepositAmount, decimal limit)
         {
-            Console.WriteLine("[Notification]: Exceeded deposit limit!"
-                              + Environment.NewLine
-                              + $"-[Current deposit amount]: {currentDepositAmount}"
-                              + Environment.NewLine
-                              + $"-[Limit]: {limit} "
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Exceeded deposit limit! Current deposit amount: {currentDepositAmount}, Limit: {limit}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyApproachingWithdrawLimit(string emailAddress, decimal currentWithdrawAmount, decimal limit)
         {
-            Console.WriteLine("[Notification]: Approaching withdraw limit!"
-                              + Environment.NewLine
-                              + $"-[Current withdraw amount]: {currentWithdrawAmount}"
-                              + Environment.NewLine
-                              + $"-[Limit]: {limit} "
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Approaching withdraw limit! Current withdraw amount: {currentWithdrawAmount}, Limit: {limit}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyExceededWithdrawLimit(string emailAddress, decimal currentWithdrawAmount, decimal limit)
         {
-            Console.WriteLine("[Notification]: Exceeded withdraw limit!"
-                              + Environment.NewLine
-                              + $"-[Current withdraw amount]: {currentWithdrawAmount}"
-                              + Environment.NewLine
-                              + $"-[Limit]: {limit} "
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Exceeded withdraw limit! Current withdraw amount: {currentWithdrawAmount}, Limit: {limit}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyApproachingTransferLimit(string emailAddress, decimal currentTransferAmount, decimal limit)
         {
-            Console.WriteLine("[Notification]: Approaching transfer limit!"
-                              + Environment.NewLine
-                              + $"-[Current transfer amount]: {currentTransferAmount}"
-                              + Environment.NewLine
-                              + $"-[Limit]: {limit} "
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Approaching transfer limit! Current transfer amount: {currentTransferAmount}, Limit: {limit}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyExceededTransferLimit(string emailAddress, decimal currentTransferAmount, decimal limit)
         {
-            Console.WriteLine("[Notification]: Exceeded transfer limit!"
-                              + Environment.NewLine
-                              + $"-[Current transfer amount]: {currentTransferAmount}"
-                              + Environment.NewLine
-                              + $"-[Limit]: {limit} "
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Exceeded transfer limit! Current transfer amount: {currentTransferAmount}, Limit: {limit}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
-        public void NotifyApproachingReceiveLimit(string emailAddress, decimal currentTransferAmount, decimal limit)
+        public void NotifyApproachingReceiveLimit(string emailAddress, decimal currentReceiveAmount, decimal limit)
         {
-            Console.WriteLine("[Notification]: Approaching receive limit!"
-                              + Environment.NewLine
-                              + $"-[Current transfer amount]: {currentTransferAmount}"
-                              + Environment.NewLine
-                              + $"-[Limit]: {limit} "
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Approaching receive limit! Current receive amount: {currentReceiveAmount}, Limit: {limit}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
-        public void NotifyExceededReceiveLimit(string emailAddress, decimal currentTransferAmount, decimal limit)
+        public void NotifyExceededReceiveLimit(string emailAddress, decimal currentReceiveAmount, decimal limit)
         {
-            Console.WriteLine("[Notification]: Exceeded receive limit!"
-                              + Environment.NewLine
-                              + $"-[Current transfer amount]: {currentTransferAmount}"
-                              + Environment.NewLine
-                              + $"-[Limit]: {limit} "
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Exceeded receive limit! Current receive amount: {currentReceiveAmount}, Limit: {limit}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyWithdrawalSuccess(string emailAddress, decimal amount)
         {
-            Console.WriteLine("[Notification]: Withdrawal successful!"
-                              + Environment.NewLine
-                              + $"-[Amount withdrawn]: {amount}"
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Withdrawal successful! Amount withdrawn: {amount}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyDepositSuccess(string emailAddress, decimal amount)
         {
-            Console.WriteLine("[Notification]: Deposit successful!"
-                              + Environment.NewLine
-                              + $"-[Amount deposited]: {amount}"
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Deposit successful! Amount deposited: {amount}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyTransferSuccessForSender(string emailAddress, decimal amount)
         {
-            Console.WriteLine($"[Notification]: Transfer of {amount} was successful. Amount debited from your account.");
-            Console.WriteLine($"-[Amount]: {amount}");
-            Console.WriteLine($"-[Recipient email]: {emailAddress}");
+            var message = $"Transfer of {amount} was successful. Amount debited from your account.";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyTransferSuccessForRecipient(string emailAddress, decimal amount)
         {
-            Console.WriteLine($"[Notification]: Transfer of {amount} was successful. Amount credited to your account.");
-            Console.WriteLine($"-[Amount]: {amount}");
-            Console.WriteLine($"-[Sender email]: {emailAddress}");
+            var message = $"Transfer of {amount} was successful. Amount credited to your account.";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyWithdrawalFailure(string emailAddress, string errorMessage)
         {
-            Console.WriteLine("[Notification]: Withdrawal failed!"
-                              + Environment.NewLine
-                              + $"-[Error message]: {errorMessage}"
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Withdrawal failed! Error message: {errorMessage}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyDepositFailure(string emailAddress, string errorMessage)
         {
-            Console.WriteLine("[Notification]: Deposit failed!"
-                              + Environment.NewLine
-                              + $"-[Error message]: {errorMessage}"
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Deposit failed! Error message: {errorMessage}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
 
         public void NotifyTransferFailure(string emailAddress, string errorMessage)
         {
-            Console.WriteLine("[Notification]: Transfer failed!"
-                              + Environment.NewLine
-                              + $"-[Error message]: {errorMessage}"
-                              + Environment.NewLine
-                              + $"-[Notifying user]: {emailAddress}");
+            var message = $"Transfer failed! Error message: {errorMessage}";
+            Console.WriteLine(NotificationTemplate, message, emailAddress);
         }
     }
 }
